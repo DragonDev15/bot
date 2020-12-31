431d3f0351e1fb23b2d9dfc6a6a49173f55fac64
Dragon_15 <70057141+DragonDev15@users.noreply.github.com>
2020-12-31 14:40:46 +1000

    Better Command Handler ~ Animal Bot Development (#1)

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,10 @@
 /* eslint-disable max-len */
 const Discord = require('discord.js');
-exports.aliases = ['h'];
-exports.name = 'help';
-exports.module = 'Info';
-exports.usage = 'help [command name]';
-exports.run = (client, message, args) =>{
+modules.exports = {
+  name = 'help';
+  category = 'Info';
+  usage = 'help [command name]';
+run: async (client, message, args) => {
   if (!args[0]) {
     const embed = new Discord.MessageEmbed()
         .setAuthor(client.user.tag, client.user.displayAvatarURL({size: 512}))
@@ -14,10 +14,10 @@ exports.run = (client, message, args) =>{
   } else if (client.commands.get(args[0]) || client.commands.find((g) => g.aliases.includes(g.name))) {
     const command = client.commands.get(args[0]) || client.commands.find((g) => g.aliases.includes(args[0]));
     const embed = new Discord.MessageEmbed()
-        .setTitle(`${command.name} command`)
+        .setTitle(`${name} command`)
         .setDescription(`Aliases: ${command.aliases.join(', ')}
-Usage: ${command.usage}
-Module: ${command.module}
+Usage: ${category}
+Module: ${module}
 `)
         .setFooter('[] - Optional; <> - Required ' + client.config.embedFooter)
         .setAuthor(client.user.tag, client.user.displayAvatarURL({size: 512}));
