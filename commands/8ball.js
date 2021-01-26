eaed2f4f25c8086bff3981d59c9b3b232026c345
matthewthechickenman <65732060+matthewthechickenman@users.noreply.github.com>
2021-01-26 20:43:03 +0000

    don't be a twat, matt

--- commands/8ball.js
+++ commands/8ball.js
@@ -24,6 +24,39 @@ module.exports = {
           .setFooter(client.config.embedFooter);
       return message.channel.send(HelpEmbed);
     } else {
+          const responses = [
+      'As I see it, yes.',
+      'Honestly, probably not',
+      'I couldn\'t say',
+      'My sources are down. Check later',
+      'No, Thank god',
+      'Yes, thankfully',
+      'No.',
+      'No - definitely.',
+      'Maybe...',
+      'Possibly, Possibly not',
+      'Cannot say now.',
+      'No, definitely.',
+      'Don\'t count on it.',
+      'It is uncertain.',
+      'It is decidedly so.',
+      'Most likely.',
+      'My reply is no.',
+      'My sources say no.',
+      'Outlook not so good.',
+      'Outlook good.',
+      'Outlook terrible.',
+      'Reply hazy, try again.',
+      'Signs point to no.',
+      'Signs point to yes.',
+      'Very doubtful.',
+      'As I see it, no.',
+      'Without a doubt.',
+      'Yes.',
+      'Yes - definitely.',
+      'You may rely on it.',
+      'Most likely not',
+    ];
       const ballEmbed = new Discord.MessageEmbed()
           .setColor('RANDOM')
           .setTitle(BallArgs + '?')
